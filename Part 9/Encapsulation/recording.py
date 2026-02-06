a62120d7dfa1fb95608a87026799ea0a7869dedc
dev_mahi <128149806+devansh-m100@users.noreply.github.com>
2026-02-06 05:33:40 +0530

    Create recording.py

---
+++ Part 9/Encapsulation/recording.py
@@ -0,0 +1,19 @@
+# WRITE YOUR SOLUTION HERE:
+class Recording:
+    def __init__(self, length):
+        if length < 0:
+            raise ValueError("The length must not be below zero")
+        self.__length = length
+
+    # A getter method
+    @property
+    def length(self):
+        return self.__length
+    
+    # A setter method
+    @length.setter
+    def length(self, length):
+        if length >= 0:
+            self.__length = length
+        else:
+            raise ValueError("The length must not be below zero")
